ad999f493b78a4cf36e0a12227fb826f7cb50ec9
lucioacri <lucioacri@gmail.com>
2025-04-07 16:29:01 +0200

    add palyndrome variation

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,23 +1,37 @@
 const phrase = prompt("Inserisci una parola", "anna");
 console.log(phrase);
 let palyndrome = true;
-const phraseArray = [];
-const invertedPhraseArray = [];
+// const phraseArray = [];
+// const invertedPhraseArray = [];
 
-for (let i = 0; i < phrase.length; i++) {
-  phraseArray.push(phrase[i]);
-}
-console.table(phraseArray);
+// for (let i = 0; i < phrase.length; i++) {
+//   phraseArray.push(phrase[i]);
+// }
+// console.table(phraseArray);
 
-for (let i = phrase.length - 1; i >= 0; i--) {
-  invertedPhraseArray.push(phrase[i]);
-}
-console.table(invertedPhraseArray);
+// for (let i = phrase.length - 1; i >= 0; i--) {
+//   invertedPhraseArray.push(phrase[i]);
+// }
+// console.table(invertedPhraseArray);
 
-for (let i = 0; i < phrase.length; i++) {
-  if (phraseArray[i] !== invertedPhraseArray[i]) {
+// for (let i = 0; i < phrase.length; i++) {
+//   if (phraseArray[i] !== invertedPhraseArray[i]) {
+//     palyndrome = false;
+//   }
+// }
+
+// console.log(palyndrome);
+
+// girafarig
+// 0 - length -1
+// 1 - length -1 -1
+// 2 - length -1 -1 -1
+
+for (let i = 0; i < phrase.length / 2 && palyndrome; i++) {
+  console.log(phrase[i]);
+
+  if (phrase[i] !== phrase[phrase.length - 1 - i]) {
     palyndrome = false;
   }
 }
-
 console.log(palyndrome);
--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -1,3 +1,22 @@
+//
+// FUNCTIONS
+//
+
+function isEvenNumber(value) {
+  // Dichiarazione di funzione
+  // Restituisco se è pari (true)
+  return value % 2 === 0;
+}
+
+// genero numero random da 1 a 5 con funzione
+function getRandonNumber(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+//
+// MAIN
+//
+
 // utente sceglie pari o dispari
 const sumChoice = prompt("Scegli pari o dispari", "pari");
 console.log(sumChoice);
@@ -6,35 +25,20 @@ console.log(sumChoice);
 const numberChoice = parseInt(prompt("Scegli un numero da 1 a 5", "3"));
 console.log(numberChoice);
 
-// genero numero random da 1 a 5 con funzione
-function randomPcNumber(max) {
-  const randomNumber = Math.floor(Math.random() * max) + 1;
-  return randomNumber;
-}
-const pcNumberFive = randomPcNumber(5);
+const pcNumberFive = getRandonNumber(5);
 console.log(pcNumberFive);
 
 // sommo i due numeri
 const sum = numberChoice + pcNumberFive;
 console.log(sum);
 
-// stabilisco se la somma è pari o dispari con funzione
-let even = false;
-function evenNumber() {
-  if (sum % 2 === 0) {
-    even = true;
-    return "Il numero è pari";
-  } else {
-    return "Il numero è dispari";
-  }
-}
-console.log(evenNumber());
+const isSumEven = isEvenNumber(sum); // Invocazione di funzione
 
 // dichiaro chi ha vinto
-if (sumChoice === "pari" && even === true) {
-  alert("Hai vinto!");
-}
-if (sumChoice === "dispari" && even === false) {
+if (
+  (sumChoice === "pari" && even === true) ||
+  (sumChoice === "dispari" && even === false)
+) {
   alert("Hai vinto!");
 } else {
   alert("Hai perso...");
