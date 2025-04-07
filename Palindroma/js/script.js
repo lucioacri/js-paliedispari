34e07bb54a0d49696677ae70dfa888b7a94022cb
lucioacri <lucioacri@gmail.com>
2025-04-07 13:58:57 +0200

    add palyndrome

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,11 +1,23 @@
 const phrase = prompt("Inserisci una parola", "anna");
 console.log(phrase);
-let palyndrome = false;
-for (let i = phrase.length - 1; i >= 0; i--) {
-  const letters = phrase[i];
-  console.log(letters);
-  if (letters === phrase) {
-    palyndrome = true;
-  } else "la parola non è palindroma";
+let palyndrome = true;
+const phraseArray = [];
+const invertedPhraseArray = [];
+
+for (let i = 0; i < phrase.length; i++) {
+  phraseArray.push(phrase[i]);
 }
+console.table(phraseArray);
+
+for (let i = phrase.length - 1; i >= 0; i--) {
+  invertedPhraseArray.push(phrase[i]);
+}
+console.table(invertedPhraseArray);
+
+for (let i = 0; i < phrase.length; i++) {
+  if (phraseArray[i] !== invertedPhraseArray[i]) {
+    palyndrome = false;
+  }
+}
+
 console.log(palyndrome);
